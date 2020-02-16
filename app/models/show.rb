3b2eb6d48418c80078fbc10fc762eabb714b0b12
ryan m <contact@ryanmeek.com>
2020-02-16 18:20:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show() self.find_by(rating: self.highest_rating) end
 
-  def self.lowest_rating
-    self.minimum(:rating)
-  end
+  def self.lowest_rating() self.minimum(:rating) end
 
   def self.least_popular_show
     self.where("rating = ?", self.lowest_rating).first
