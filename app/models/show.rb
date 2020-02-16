2891bc208e37fc67b7acdabfab96e7e708f56ef3
ryan m <contact@ryanmeek.com>
2020-02-16 18:25:44 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,17 +1,17 @@
 class Show < ActiveRecord::Base
 
-  def self.highest_rating() self.maximum(:rating) end
+  def highest_rating() self.maximum(:rating) end
 
-  def self.most_popular_show() self.find_by(rating: self.highest_rating) end
+  def most_popular_show() self.find_by(rating: self.highest_rating) end
 
-  def self.lowest_rating() self.minimum(:rating) end
+  def lowest_rating() self.minimum(:rating) end
 
-  def self.least_popular_show() self.find_by(rating: self.lowest_rating) end
+  def least_popular_show() self.find_by(rating: self.lowest_rating) end
 
-  def self.ratings_sum() self.sum(:rating) end
+  def ratings_sum() self.sum(:rating) end
 
-  def self.popular_shows() self.where("rating > 5") end
+  def popular_shows() self.where("rating > 5") end
 
-  def self.shows_by_alphabetical_order() self.order(:name) end
+  def shows_by_alphabetical_order() self.order(:name) end
 
 end
