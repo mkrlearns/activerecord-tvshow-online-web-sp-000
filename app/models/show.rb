e46c9e207507520751a508876a196293abc33215
ryan m <contact@ryanmeek.com>
2020-02-16 18:26:42 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,14 +4,14 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show() find_by(rating: highest_rating) end
 
-  def self.lowest_rating() self.minimum(:rating) end
+  def self.lowest_rating() minimum(:rating) end
 
-  def self.least_popular_show() self.find_by(rating: self.lowest_rating) end
+  def self.least_popular_show() find_by(rating: lowest_rating) end
 
-  def self.ratings_sum() self.sum(:rating) end
+  def self.ratings_sum() sum(:rating) end
 
-  def self.popular_shows() self.where("rating > 5") end
+  def self.popular_shows() where("rating > 5") end
 
-  def self.shows_by_alphabetical_order() self.order(:name) end
+  def self.shows_by_alphabetical_order() order(:name) end
 
 end
