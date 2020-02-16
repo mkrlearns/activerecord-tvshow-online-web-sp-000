86a0127390bb0c7fac9947892b70ee0c0aea7c49
ryan m <contact@ryanmeek.com>
2020-02-16 18:20:25 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,9 +2,7 @@ class Show < ActiveRecord::Base
 
   def self.highest_rating() self.maximum(:rating) end
 
-  def self.most_popular_show
-    self.find_by(rating: self.highest_rating)
-  end
+  def self.most_popular_show() self.find_by(rating: self.highest_rating) end
 
   def self.lowest_rating
     self.minimum(:rating)
